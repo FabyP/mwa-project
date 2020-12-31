712791d1e8d93687d889c129b4f4c87d77b6b54a
unknown <melvin.schmidt92@googlemail.com>
2020-12-31 23:46:09 +0100

    add model preferences

---
+++ app/src/main/java/com/example/mwaproject/SettingsActivity.java
@@ -0,0 +1,13 @@
+package com.example.mwaproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SettingsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
